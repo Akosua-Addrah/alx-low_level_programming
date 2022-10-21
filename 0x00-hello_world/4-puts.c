21de0bb9f2153c9d1b3c9a5738df13c28ffc4445
Akosua-Addrah <millicent.addrah@gmail.com>
2022-10-21 07:39:52 -0700

    mine

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**   
+ * main - output contents
+ * Return: always return zero 
+ *
+ */
+
+int main(void)
+{ 
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}
+
