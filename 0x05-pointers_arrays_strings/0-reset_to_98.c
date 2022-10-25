c5fcb1f1b9a49edad6a1af68b47f7efea67da3b4
Akosua-Addrah <millicent.addrah@gmail.com>
2022-10-24 20:05:00 -0700

    message

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98- starting point
- * @n: pointer to 98
- * Return: always zero
- */
+* reset_to_98- starting point
+* @n: pointer to 98
+* Return: always zero
+**/
 
 void reset_to_98(int *n)
 {
